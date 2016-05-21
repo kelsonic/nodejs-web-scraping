f5ba4a66d6b990302f7de2f018b1606c7f399c93
Kelson Adams <kgadams93@gmail.com>
2016-05-21 14:28:49 -0700

    Start building pinterest web scraper

--- ex2.js
+++ ex2.js
@@ -6,5 +6,32 @@ var fs = require('fs');
 var app = express();
 var port = 8000;
 
+var url = "http://www.indeed.com/cmp/Fuze-Lab/jobs/Entry-Junior-PHP-Jquery-MySQL-Coder-Team-Member-01790db21236725e"
+
+request(url, function(err, res, body) {
+  var $ = cheerio.load(body);
+  var companyName = $('.company');
+  var companyNameText = companyName.text();
+
+  var jobTitle = $(".jobtitle-font");
+  var jobTitleText = jobTitle.text();
+
+  var location = $('.location')
+  var locationText = location.text();
+
+  var summary = $('#job_summary p');
+  var summaryText = summary.text();
+
+  var job = {
+    jobTitle: jobTitleText,
+    location: locationText,
+    companyName: companyNameText,
+    summary: summaryText
+  }
+
+  console.log(job);
+
+})
+
 app.listen(port);
 console.log("server is running on " + port)
---
+++ pinterest.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var app = express();
+var port = 8000;
+
+request(url, function(err, res, body) {
+  var url = "https://www.pinterest.com/pin/"
+})
+
+app.listen(port);
+console.log("server is running on " + port)
