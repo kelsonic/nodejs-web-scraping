3101bb3700cf846b23cd88207b1acf642e2a700e
kelsonic <kgadams93@gmail.com>
2016-05-25 11:57:17 -0700

    Create schema with title, description, image, and timestamps so users can save scrapes

---
+++ item.model.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var itemSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  }
+  description: {
+    type: String,
+    required: true
+  }
+  image: {
+    type: String,
+    required: true
+  }
+  timeStamp: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('item', ItemSchema);
