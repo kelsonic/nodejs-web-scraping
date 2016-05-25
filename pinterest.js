eb20b4caf27cd2755dc7418ee3bb0e74c7a98f70
kelsonic <kgadams93@gmail.com>
2016-05-25 11:01:24 -0700

    Create web scraper for a particular pinterest page

--- pinterest.js
+++ pinterest.js
@@ -3,12 +3,23 @@ var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var app = express();
 var port = 8000;
 
-request(url, function(err, res, body) {
-  var url = "https://www.pinterest.com/pin/"
-})
+var url = "https://www.pinterest.com/pin/16395986122172500/";
 
-app.listen(port);
-console.log("server is running on " + port)
+request(url, function(err, res, body) {
+  
+  var pin = {};
+  var $ = cheerio.load(body);
+
+  var img = $("meta[itemprop = 'image']").get(1);
+  var $img = $(img).attr('content');
+  var $desc = $("meta[itemprop = 'text']").attr('content');
+
+  var pin = {
+    img: $img,
+    desc: $desc,
+    url: url
+  }
+  console.log("scraped: ", pin);
+})
