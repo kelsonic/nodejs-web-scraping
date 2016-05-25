a88fbfb996a967cadb6483e9371b3be784ccee33
kelsonic <kgadams93@gmail.com>
2016-05-25 12:23:22 -0700

    Require routes

--- pinScraper/server.js
+++ pinScraper/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var routes = require('./routes');
 
 var port = 8080;
 mongoose.connect('mongodb://localhost/pinScraper');
@@ -14,6 +15,8 @@ app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', routes);
 
 app.listen(port, function() {
   console.log('running server on port ' + port);
