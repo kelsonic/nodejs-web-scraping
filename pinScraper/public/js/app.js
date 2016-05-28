03223d3b0e2662b3895782a7ad4d65351cfd0281
kelsonic <kgadams93@gmail.com>
2016-05-28 11:22:34 -0700

    Fix config to render main partial and add scrape functionality to controllers

--- pinScraper/public/js/app.js
+++ pinScraper/public/js/app.js
@@ -1,22 +1,22 @@
 (function() {
-  "use strict";
+  'use strict';
 
   angular
-    .module("app", [
+    .module('app', [
       'ui.router'
-    ])
+      ])
     .config(config)
 
     config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
 
     function config($stateProvider, $urlRouterProvider, $locationProvider) {
       $stateProvider
-        .state("main", {
-          url: "/",
-          templateUrl: "../partials/main.html",
-          controller: "MainCtrl"
-        });
-        $urlRouterProvider.otherwise("/");
-        $locationProvider.html5mode(true);
+      .state('main', {
+        url: '/',
+        templateUrl: '../partials/main.html',
+        controller: 'MainCtrl'
+      });
+      $urlRouterProvider.otherwise('/');
+      $locationProvider.html5Mode(true);
     }
 })();
