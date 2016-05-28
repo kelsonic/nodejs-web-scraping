782f257dd94dc9d75e069d76ede55c31283c0ea6
kelsonic <kgadams93@gmail.com>
2016-05-28 10:48:31 -0700

    Create partials, routes, and public assets

---
+++ pinScraper/public/js/controllers.js
@@ -0,0 +1,13 @@
+(function() {
+  'use static';
+
+  angular
+    .module('app')
+    controller('MainCtrl', MainCtrl);
+
+    MainCtrl.$inject = ['$scope', '$http'];
+
+    function MainCtrl($scope, $http) {
+      
+    }
+})();
--- pinScraper/routes.js
+++ pinScraper/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var item = require('./models/Item.model');
 
 router.get('/test', function(req, res) {
   res.send('hello from api/test');
