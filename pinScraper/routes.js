77e7434f68c631fbf8484615ccf113010250abf2
kelsonic <kgadams93@gmail.com>
2016-05-28 11:35:44 -0700

    Handle user input from pinterest and raise errors if url submitted is not from pinterest

--- pinScraper/routes.js
+++ pinScraper/routes.js
@@ -1,9 +1,40 @@
 var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 var router = express.Router();
 var item = require('./models/Item.model');
 
-router.get('/test', function(req, res) {
-  res.send('hello from api/test');
+router.post('/scrape', function(req, res) {
+  var url = req.body.url;
+  
+  if (url.indexOf("pinterest") > -1) {
+    request(url, function(err, resp, body) {
+      console.log(url);
+
+      if (err) {
+        console.log('error scraping ');
+      } else {
+        var pin = {};
+        var $ = cheerio.load(body);
+
+        var img = $("meta[itemprop = 'image']").get(1);
+        var $img = $(img).attr('content');
+        var $desc = $("meta[itemprop = 'text']").attr('content');
+
+        var pin = {
+          img: $img,
+          desc: $desc,
+          url: url
+        }
+
+        console.log('scraped: ', pin);
+        res.json(pin);
+      }
+    });
+  } else {
+    console.log('cannot locate scraper');
+  }
 })
 
 module.exports = router;
